352780aa06bcd1b3f79a7ef64c16795dc2611c3a
xan978 <xan9788@gmail.com>
2025-02-27 06:08:00 -0500

    added things

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from board import Board
 import os
 # from logic import Piece
 
+os.system("clear")
 game = Board()
 game.print_board()
+
+# def clear():
+#    os.system("cls" if os.name == "nt" else "clear")
 
 pos = []
 x = input("put in x: ")
@@ -11,7 +15,7 @@ y = input("put in y: ")
 pos.append(int(x))
 pos.append(int(y))
 
-if os.name == 'nt': os.system('cls') else: os.system('clear')
+os.system("clear")
 game.add_piece(icon="*", type=10, pos=pos, color=1)
 game.print_board()
 
