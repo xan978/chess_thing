352da250bebeb1d71327665c6a59b8ab06b52f01
xan978 <xan9788@gmail.com>
2025-02-27 05:55:37 -0500

    e

--- board.py
+++ board.py
@@ -15,21 +15,24 @@ class Board:
             for x in range(8):
                 print(f"[{self.grid[i][x][0]}] ", end="")
             print()
-        print(f"  1   2   3   4   5   6   7   8")
+        print(f"  1   2   3   4   5   6   7   8 \n")
 
     def board_setup(self):
         pass
 
     def add_piece(self, icon, type, pos, color):
         pos = self.pos_conversion(pos)
-        self.grid.append[pos[0]][pos[1][0]](icon)
-        self.grid.append[pos[0]][pos[1][1]](type)
-        self.grid.append[pos[0]][pos[1][2]](color)
+        x = pos[0]
+        y = pos[1]
+        self.grid[y][x][0] = icon
+        self.grid[y][x][1] = type
+        self.grid[y][x][2] = color
 
     def pos_conversion(self, pos):
         new_pos = []
         new_pos.append(pos[0] - 1)
         new_pos.append(8 - pos[1])
+        return new_pos
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from board import Board
+import os
 # from logic import Piece
 
 game = Board()
+game.print_board()
+
+pos = []
+x = input("put in x: ")
+y = input("put in y: ")
+pos.append(int(x))
+pos.append(int(y))
+
+if os.name == 'nt': os.system('cls') else: os.system('clear')
+game.add_piece(icon="*", type=10, pos=pos, color=1)
 game.print_board()
 
 
