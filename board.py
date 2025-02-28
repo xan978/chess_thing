f3fa5d5bb48e7f05be044fb300a557c1936603f6
xan978 <xan9788@gmail.com>
2025-02-28 09:27:47 -0500

    updated journal and added more stuff

--- board.py
+++ board.py
@@ -17,22 +17,56 @@ class Board:
             print()
         print(f"  1   2   3   4   5   6   7   8 \n")
 
-    def board_setup(self):
-        pass
-
-    def add_piece(self, icon, type, pos, color):
+    def add_piece(self, type, pos, color):
         pos = self.pos_conversion(pos)
         x = pos[0]
         y = pos[1]
+        icon = self.get_icon(type=type, color=color)
         self.grid[y][x][0] = icon
         self.grid[y][x][1] = type
         self.grid[y][x][2] = color
+
+    def remove_piece(self, pos):
+        self.add_piece(type=0, pos=pos, color=0)
 
     def pos_conversion(self, pos):
         new_pos = []
         new_pos.append(pos[0] - 1)
         new_pos.append(8 - pos[1])
         return new_pos
+
+    def board_setup(self):
+        for i in range(8):
+            self.add_piece(type=1, pos=[i+1, 2], color=1)
+        for i in range(8):
+            self.add_piece(type=1, pos=[i+1, 7], color=2)
+        # self.add_piece()
+
+    def get_icon(self, type, color):
+        # 1=pawn 2=rook 3=knight 4=bishop 5=queen 6=king 7=test_piece
+        # 1=white 2=black
+        data_sheet = {
+            (0, 0): " ",
+            (1, 1): "P",
+            (1, 2): "p",
+            (2, 1): "R",
+            (2, 2): "r",
+            (3, 1): "k",
+            (3, 2): "K",
+            (4, 1): "B",
+            (4, 2): "b",
+            (5, 1): "Q",
+            (5, 2): "q",
+            (6, 1): "K",
+            (6, 2): "k",
+            (7, 1): "*",
+            (7, 2): "*"
+        }
+        return data_sheet.get((type, color), "?")
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -2,28 +2,39 @@ from board import Board
 import os
 # from logic import Piece
 
+game = Board()
+game.board_setup()
+
 def clear():
    os.system("cls" if os.name == "nt" else "clear")
 
-game = Board()
+def cord_input():
+    user_input_pos = input("enter cordinate in x,y format: ")
+    pos_string = user_input_pos.split(",")
+    pos = list(map(int, pos_string))
+    return pos
 
 while True:
     clear()
     game.print_board()
-    mode = int(input("| add piece: 1 | move piece: 2 | delete piece: 3 |\n"))
+    mode = int(input("| add piece: 1 | move piece: 2 | delete piece: 3 | quit: 4 |\n"))
 
     if mode == 1:
         clear()
         game.print_board()
-        pos = []
-        user_input_piece = int(input("enter piece number: \n"))
-        user_input_pos = int(input("enter xy cordinate: "))
+        user_input_type = int(input("enter piece number: "))
+        user_input_color = int(input("enter piece color, 1=white 2=black: "))
+        pos = cord_input()
+        game.add_piece(type=user_input_type, color=user_input_color, pos=pos)
 
     elif mode == 2:
         pass
 
     elif mode == 3:
-        pass
+        clear()
+        game.print_board()
+        pos = cord_input()
+        game.remove_piece(pos)
 
     else:
         print("bye!")
